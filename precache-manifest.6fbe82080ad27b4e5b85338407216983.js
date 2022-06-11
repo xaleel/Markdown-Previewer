974251c81ac9fa29a5869a2224adbe6afe39601a
Xaleel <xaleelchess@gmail.com>
2022-06-11 14:55:13 +0300

    Updates

---
+++ precache-manifest.6fbe82080ad27b4e5b85338407216983.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "50da8478955418e3ae3c",
+    "url": "/markdown/static/js/main.06d06543.chunk.js"
+  },
+  {
+    "revision": "1ef3ee70a0d1f62d02c7",
+    "url": "/markdown/static/js/runtime~main.7d448bfc.js"
+  },
+  {
+    "revision": "1cdd0475287d0f86dcf9",
+    "url": "/markdown/static/js/2.168b5bbc.chunk.js"
+  },
+  {
+    "revision": "4f4650df98a8257d83d29ad69e97ba4e",
+    "url": "/markdown/index.html"
+  }
+];
